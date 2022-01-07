616f57290230459a63c55d7a3890a69a339bcedd
Archit Yadav <archityadav97@gmail.com>
2022-01-07 18:51:36 +0100

    Create functions for vectorized and non-vectorized solutions

--- voxcarv3D.py
+++ voxcarv3D.py
@@ -28,27 +28,22 @@ X, Y, Z = np.mgrid[-1:1:step, -1:1:step, -0.5:0.5:step]  # Voxel coordinates
 occupancy = np.ndarray(shape=(resolution, resolution, int(resolution / 2)), dtype=int)  # Voxel occupancy
 occupancy.fill(1)  # Voxels are initially occupied then carved with silhouette information
 
-####### MAIN #########  
-if __name__ == "__main__":
 
+def compute_grid_non_vec(occupancy):
+    """
+    Non-vectorized solution
+    :return:
+    """
     i = 1
-    myFile = "image{0}.pgm".format(i)  # read the input silhouettes
+    myFile = "./img/image{0}.pgm".format(i)  # read the input silhouettes
     print(myFile)
     img = mpimg.imread(myFile)
     if img.dtype == np.float32:  # if not integer
         img = (img * 255).astype(np.uint8)
 
-    print(img.shape)
-    print(calib.shape)
-    print(X.shape)
-    print(occupancy.shape)
-
-    # Compute grid projection in images
-
     for x in range(X.shape[0]):
         for y in range(X.shape[1]):
             for i in range(X.shape[2]):
-
                 result = np.dot(calib[0].reshape(3, 4), np.array([X[x][y][i], Y[x][y][i], Z[x][y][i], 1]))
                 u = int(result[0] / result[2])
                 v = int(result[1] / result[2])
@@ -58,6 +53,49 @@ if __name__ == "__main__":
                 elif img[u][v] == 0:
                     # Update grid occupancy
                     occupancy[x][y][i] = 0
+
+    return occupancy
+
+
+def compute_grid_vec(occupancy):
+    """
+    Edmond's vectorized solution
+    :return: Modified voxel occupancy
+    """
+    # For each image
+    for i in range(1):
+        myFile = "./img/image{0}.pgm".format(i)  # read the input silhouettes
+        print(myFile)
+        img = mpimg.imread(myFile)
+        if img.dtype == np.float32:  # if not integer
+            img = (img * 255).astype(np.uint8)
+
+        w = calib[i][8] * X + calib[i][9] * Y + calib[i][10] * Z + calib[i][11]
+        u = calib[i][0] * X + calib[i][1] * Y + calib[i][2] * Z + calib[i][3] / w
+        v = calib[i][4] * X + calib[i][5] * Y + calib[i][6] * Z + calib[i][7] / w
+        u = u.astype(int)
+        v = v.astype(int)
+        u[u >= 300] = 299
+        v[v >= 300] = 299
+        u[u < 0] = 0
+        v[v < 0] = 0
+
+        # Update grid occupemcy
+        img[img > 0] = 1
+        occupancy = occupancy * img[v, u]
+        return occupancy
+
+
+####### MAIN #########
+if __name__ == "__main__":
+    # print(img.shape)
+    # print(calib.shape)
+    # print(X.shape)
+    # print(occupancy.shape)
+
+    # Compute grid projection in images
+    # occupancy = compute_grid_non_vec(occupancy)
+    occupancy = compute_grid_vec(occupancy)
 
     # Voxel visualization
     verts, faces, normals, values = measure.marching_cubes(occupancy, 0.25)  # Marching cubes
